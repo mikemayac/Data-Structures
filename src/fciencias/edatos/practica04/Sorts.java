7a78529a089f5ce654092cbc38263832a87e1f59
Francisco Becerril <franciscobecerril@PC-Paquito.bwrouter>
2021-11-14 00:21:50 -0600

    quick

--- src/fciencias/edatos/practica04/Sorts.java
+++ src/fciencias/edatos/practica04/Sorts.java
@@ -2,7 +2,7 @@ package fciencias.edatos.practica04;
 
 import java.util.Arrays;
 
-
+import java.util.Random;
 /**
  * Implementación de algunos algoritmos de ordenamiento.
  * @author Francisco Javier Becerril Lara No Cuenta 317114490 y Joel Miguel Maya Castrejón 417112602
@@ -27,7 +27,7 @@ public class Sorts{
   * @param arr el arreglo a ordenar.
    */
   public static void quickSort(int[]arr){
-    quickSort(arr,0,arr.length -1);
+    quickSort(arr,0,arr.length-1);
   }
   /**
   * Metodo privado quickSort
@@ -39,8 +39,8 @@ public class Sorts{
 		 if(hi<=lo){
 			 return ;
 		 }
-    int j = quicksort(arr,lo,hi);
-     quickSort(arr,lo,j-1);
+    int j = partition(arr,lo,hi);
+     quickSort(arr,lo,j);
      quickSort(arr,j+1,hi);
    }
    /**
@@ -49,23 +49,29 @@ public class Sorts{
    * @param lo entero.
    * @param hi entero.
     */
-   private static int quicksort(int[] arr, int lo, int hi){
+   private static int partition(int[] arreglo, int lo, int hi){
     int i = lo;
-    int j = hi+1;
-    int piv = arr[lo];
-    while(true){
-      while(arr[i++]<piv){
-        if(i==hi) break;
-      }
-      while(piv<arr[j--]){
-        if(j==lo) break;
-      }
-      if(i>=j) break;
-    }
-    swap(arr,i,j);
-    swap(arr,lo,j);
-    return j;
-   }
+    int j = hi;
+    int piv = arreglo[lo];
+		while (true) {
+			while (arreglo[i] < piv) {
+					i++;
+			}
+			while (arreglo[j] > piv) {
+					j--;
+			}
+			if (i >= j) {
+					return j;
+			} else {
+					int temp = arreglo[i];
+					arreglo[i] = arreglo[j];
+					arreglo[j] = temp;
+					i++;
+					j--;
+			}
+
+	}
+}
 
 
 	/**
@@ -96,7 +102,7 @@ public class Sorts{
 	 * @param arr el arreglo donde buscar.
 	 * @param e el elemento a buscar.
 	 */
-	public int find(int[] arr, int e){
+	public static int find(int[] arr, int e){
 		return contains(arr, e, 0, arr.length-1);
 	}
 
@@ -124,7 +130,6 @@ public class Sorts{
 
 			mergeSort(arr, lo, mid);
 			mergeSort(arr, mid+1, hi);
-
 			merge(arr, lo, mid, hi);
 		}
 
@@ -144,14 +149,62 @@ public class Sorts{
 			for(int k = lo ; k <= hi; k++){
 				// Si ya nos acabamos los elementos de la primera mitad
 				if(i > mid)
-					arr[k] = aux2[j++];
-				else if(j > hi) // Si ya nos acabamos la segunda mitad
-					arr[k] = aux1[i++];
-				else if(aux2[j] < aux1[i]) // El menor está en la primera mitad
-					arr[k] = aux2[j++];
-				else // El mayor está en la segunda mitad
-					arr[k] = aux1[i++];
+			arr[k] = aux2[j++];
+		else if(j > hi) // Si ya nos acabamos la segunda mitad
+			arr[k] = aux1[i++];
+		else if(aux2[j] < aux1[i]) // El menor está en la primera mitad
+			arr[k] = aux2[j++];
+		else // El manor está en la segunda mitad
+			arr[k] = aux1[i++];
 			}
 		}
+		/**
+		 * Crea un nuevo arreglo con números pseudoaleatorios.
+		 * @param n el tamaño del arreglo a crear.
+		 * @param max el mayor elemento a generar en el arreglo.
+		 * @return un arreglo de tamaño n con números pseudoaleatorios de 0 a 19.
+		 */
+		public static int[] generate(int n, int max){
+			int[] res = new int[n];
+			Random rn = new Random();
+			for(int i = 0 ; i < n; i++)
+				res[i] = rn.nextInt(max);
+			return res;
+		}
 
+		public static void main(String[] args) {
+			int[] arr1 = generate(100, 99);
+			int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+			int [] arr3 =  {100,99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58
+				,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,
+				8,7,6,5,4,3,2,1};
+			//System.out.println("No ordenado: " + Arrays.toString(arr1));
+
+			long inicio = System.currentTimeMillis();
+			quickSort(arr1);
+			System.out.println("Despues de QS: " + Arrays.toString(arr1));
+
+			long fin = System.currentTimeMillis();
+
+			System.out.println("Ordenado con quickSort tardó: " + (fin - inicio) + " milisegundos");
+
+
+
+			inicio = System.currentTimeMillis();
+			quickSort(arr2);
+				System.out.println("Despues de QS: " + Arrays.toString(arr2));
+			fin = System.currentTimeMillis();
+
+			System.out.println("Ordenado con quickSort tardó: " + (fin - inicio) + " milisegundos");
+
+
+			//System.out.println("No ordenado: " + Arrays.toString(arr3));
+			inicio = System.currentTimeMillis();
+			quickSort(arr3);
+		   find(arr3,1);
+			 	System.out.println("Después de find: " + find(arr3,23)+"");
+			fin = System.currentTimeMillis();
+			//System.out.println("Ordenado: " + Arrays.toString(arr3));
+			System.out.println("Ordenado con  find tardó: " + (fin - inicio) + " milisegundos");
+		}
 }
